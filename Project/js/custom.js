cacc9260fb8a2344abdd3ea1be5f7f53a7444de3
JacobZigby <jibz1998@gmail.com>
2017-05-15 11:43:11 -0400

    Update custom.js
    One fucking hours spent on MUTED!!!! MUTED!!!!

--- Project/js/custom.js
+++ Project/js/custom.js
@@ -1,6 +1,30 @@
 // Offset for Site Navigation
-$('#siteNav').affix({
-	offset: {
-		top: 100
+
+for(var index=1; index<=4; index++){
+	var ref="#img" + index;
+	$(ref).mouseover(grow);
+	$(ref).mouseout(shrink);
+};
+function grow(){
+	$(this).animate({
+		height: "+=10",
+		width: "+=10"
+	})
+};
+function shrink(){
+	$(this).animate({
+		height: "-=10",
+		width: "-=10"
+	})
+};
+function mute(){
+	var ref=document.getElementById("music");
+	if(ref.muted==true){
+		ref.muted=false;
+		document.getElementById("mute").textContent="Mute";
 	}
-})
+	else{
+		ref.muted=true;
+		document.getElementById("mute").textContent="UnMute";
+	}
+}
